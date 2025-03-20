de8491cb34c5db0e6e7e0d3c0ff1c4d8e82ab79e
mike <mike@mike.com>
2025-03-20 13:52:50 +0800

    chore: update file

--- apps/api/src/config/config.production.ts
+++ apps/api/src/config/config.production.ts
@@ -19,7 +19,7 @@ export default {
     secret: process.env.JWT_SECRET || '123456',
   },
   redis: {
-    host: process.env.REDIS_HOST || '127.0.0.1', // default value
+    host: process.env.REDIS_HOST || 'redis', // default value
     port: process.env.REDIS_PORT || 6379, // default value
     password: process.env.REDIS_PASSWORD || '123456',
     db: process.env.REDIS_DB || 10,
