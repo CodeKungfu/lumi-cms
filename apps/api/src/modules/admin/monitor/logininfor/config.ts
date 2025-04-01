cf053745b2a42a203ed36698d07debe58e3bd892
mike <mike@mike.com>
2025-04-01 16:25:51 +0800

    refactor(monitor): Change keyStr value

--- apps/api/src/modules/admin/monitor/logininfor/config.ts
+++ apps/api/src/modules/admin/monitor/logininfor/config.ts
@@ -1,7 +1,7 @@
 import { sys_logininfor } from '@repo/database';
 import { ADMIN_PREFIX } from 'src/modules/admin/admin.constants';
 export { ADMIN_PREFIX };
-export const keyStr = '操作日志';
+export const keyStr = '登录日志';
 export const tableName = 'sys_logininfor';
 export const controllerName = 'logininfor';
 export type tableType = sys_logininfor;
